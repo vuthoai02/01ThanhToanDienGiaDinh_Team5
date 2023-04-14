402f6b3005b2cf4e3928f7d1642695e63d94169f
vuthoai <vuthoaivt@gmail.com>
2023-04-14 09:47:50 +0700

    thay doi URL api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000";
+const URL = "https://eenergy.onrender.com";
 
 /*=================User===========================*/
 export const fetchUser = (payload) => axios.post(`${URL}/users/login`, payload);
